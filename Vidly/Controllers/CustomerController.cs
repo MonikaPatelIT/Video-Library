4265865913cf27d0ad01bb19c00138c8b9599fe4
Monika Patel <monikapatel.it@outlook.com>
2019-03-08 02:02:04 +1300

    First commit

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.ViewModel;
+using Vidly.Models;
+namespace Vidly.Controllers
+{
+    [RoutePrefix("Customer")]
+    [Route("{action=Index}")]
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+        //Get: Customer/Details/Id
+        [Route("Details/{id}")]
+        public ActionResult Details(int Id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer {Id=1, Name ="Monika Patel"},
+                new Customer {Id=2, Name ="Hiren Patel"}
+            };
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    [RoutePrefix("Movie")]
+    [Route("{action=index}")]
+    public class MovieController : Controller
+    {
+
+        //Get: Moive/Index
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        // GET: Moive/Random
+        public ActionResult Random()
+        {
+            var movie = new List<Movie>
+            {
+                new Movie(){ Name = "Bagban" }
+            };
+            var customers = new List<Customer>
+          {
+              new Customer(){Name="Monika"},
+              new Customer(){Name="Hiren"}
+          };
+            var randomView = new RandomViewModel
+            {
+                Movies = movie,
+                Customers = customers
+            };
+            return View(randomView);
+        }
+
+
+        [Route("Details/{id}")]
+        public ActionResult MovieById(int Id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == Id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                return View(movie);
+            }
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1,Name ="Hera pheri"},
+                new Movie {Id=2, Name ="Phir Hera pheri"}
+            };
+
+        }
+      
+
+       
+    }
+}
